489418ee821a3330c77930543d563750e705b49b
thingseong <audtjd1893@gmail.com>
2022-05-08 19:33:33 +0900

    feat: add logo, nightmode btn
    - rotate nightmode btn
    - No event

--- src/page/main.ts
+++ src/page/main.ts
@@ -1,8 +1,19 @@
+import Header from "../component/header/header";
 import Component from "../core/Component";
 
+<<<<<<< HEAD
 class Main extends Component {
   template() {
     return "<div id='main_page'></div>";
+=======
+class Main extends Component{
+  template(){
+    return `<div data-component='Header'></div>`;
+  }
+  mounted(){
+    const $header = this.$target.querySelector('[data-component="Header"]');
+    new Header($header, {});
+>>>>>>> db879c9 (feat: add logo, nightmode btn)
   }
 }
 
