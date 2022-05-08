8c433f8af75cdc52c6dfef8e4c7cc968573e482a
thingseong <audtjd1893@gmail.com>
2022-05-08 19:46:57 +0900

    feat: nightmode click event
    - add click event

--- src/component/header/nightmode.ts
+++ src/component/header/nightmode.ts
@@ -14,7 +14,7 @@ export default class NightMode extends Component{
       if(mode){
           return `
             <div class="nightmode">
-                <svg width='24' height='24' id='nightmodesvg'>
+                <svg width='24' height='24'>
                 <path d="M20.6144 14.6145C19.787 14.8653 18.9093 15.0001 18 15.0001C13.0294 15.0001 9 10.9707 9 6.00013C9 5.09088 9.13484 4.21311 9.3856 3.38574C5.69007 4.50583 3 7.93883 3 12.0001C3 16.9707 7.02944 21.0001 12 21.0001C16.0613 21.0001 19.4943 18.3101 20.6144 14.6145Z"></path>
                 </svg>
             </div>
@@ -29,6 +29,13 @@ export default class NightMode extends Component{
             </div>
       `
       }
+  }
+
+  setEvent(){
+      this.addEvent('click', '.nightmode', ()=>{
+          const night = this.state.night;
+          this.setState({night: !night})
+      })
   }
   
 }
