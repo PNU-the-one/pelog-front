c776d3a09090c8d890fc900acc214fe03eb79a3e
thingseong <audtjd1893@gmail.com>
2022-05-08 21:23:59 +0900

    feat: add all header btns
    - finish css
    - should add event

--- src/component/header/header.ts
+++ src/component/header/header.ts
@@ -1,6 +1,9 @@
 import Component from "../../core/Component";
+import Login from "./login";
 import Logo from "./logo";
 import NightMode from "./nightmode";
+import Search from "./search";
+import "./header.css";
 
 export default class Header extends Component{
   setup(){
@@ -10,9 +13,12 @@ export default class Header extends Component{
     return `
       <div id='header'>
         <div data-component='Logo'></div>
+        
+        <div data-component='Login'></div>
+        <div data-component='Search'></div>
         <div data-component='NightMode'></div>
-        <div data-component='search'></div>
-        <div data-component='login'></div>
+        
+        
         <div data-component='profile'></div>
       </div>
     `
@@ -20,7 +26,13 @@ export default class Header extends Component{
   mounted(){
     const $logo = this.$target.querySelector('[data-component="Logo"]');
     const $nightmode = this.$target.querySelector('[data-component="NightMode"]')
+    const $login = this.$target.querySelector('[data-component="Login"]');
+    const $search = this.$target.querySelector('[data-component="Search"]');
+    
+    
     new Logo($logo, {});
     new NightMode($nightmode, {});
+    new Login($login, {});
+    new Search($search, {});
   }
 }
