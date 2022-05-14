d4055d7c04497fbbce9e1fc53f3776ee94c728a3
vgihan <jum654@naver.com>
2022-05-14 23:27:06 +0900

    fix: 빈 페이지로 변경

--- src/page/main.ts
+++ src/page/main.ts
@@ -1,17 +1,8 @@
-import LoginModal from "component/LoginModal";
-import Modal from "component/Modal";
 import Component from "../core/Component";
 
 class Main extends Component {
   template() {
-    return "<div id='main_page'>Hello World !</div>";
-  }
-
-  mounted(): void {
-    const $portal = document.getElementById("portal");
-    const modal = new Modal($portal);
-    const $modalFrame = $portal.querySelector(".modal_frame");
-    const loginModal = new LoginModal($modalFrame);
+    return "<div id='main_page'></div>";
   }
 }
 
