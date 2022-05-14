5d026073b14932cb2aedbd9871d1b6ae18bb238b
vgihan <jum654@naver.com>
2022-05-14 18:39:24 +0900

    feat: LoginModal 컴포넌트 구현
    - flexbox를 활용하여 loginModal의 left 영역과 right 영역을 분리.
    - github, google, facebook image를 아직 얻지 못함.
    - 회원가입 버튼 클릭 시 임시로 # 경로로 이동하도록 함.

---
+++ src/component/LoginModal/index.ts
@@ -0,0 +1,37 @@
+import Component from "core/Component";
+import "./index.css";
+
+class LoginModal extends Component {
+  template(): string {
+    return `<div class='login_modal'>
+      <div class='login_modal_left'>
+        <img src='' />
+        <p>환영합니다!</p>
+      </div>
+      <div class='login_modal_right'>
+        <button class='cancel_button'>X</button>
+        <div class='login_modal_title'>
+          <p>로그인</p>
+        </div>
+        <div class='login_modal_content'>
+          <p>이메일로 로그인</p>
+          <div class='login_modal_input'>
+            <input type='text' placeholder='이메일을 입력하세요.' />
+            <button class='login'>로그인</button>
+          </div>
+        </div>
+        <div class='login_modal_content'>
+          <p>소셜 계정으로 로그인</p>
+          <div class='login_modal_input'>
+            <button class='github_login'>git</button>
+            <button class='google_login'>goo</button>
+            <button class='facebook_login'>fac</button>
+          </div>
+        </div>
+        <p>아직 회원이 아니신가요?<a href="#">회원가입</a></p>
+      </div>
+    </div>`;
+  }
+}
+
+export default LoginModal;
