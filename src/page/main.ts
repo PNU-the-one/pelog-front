5e88a72e4e4dbb8a1b5bffbd6cc4f3c6f1633e37
thingseong <audtjd1893@gmail.com>
2022-05-21 01:07:25 +0900

    merge main branch

--- src/page/main.ts
+++ src/page/main.ts
@@ -1,27 +1,13 @@
 import Board from "component/Board/board";
 import Component from "../core/Component";
 
-<<<<<<< HEAD
-class Main extends Component {
-  template() {
-    return "<div id='main_page'></div>";
-=======
 class Main extends Component{
   template(){
     return `<div data-component='Board'></div>`;
   }
   mounted(){
-<<<<<<< HEAD
-    const $header = this.$target.querySelector('[data-component="Header"]');
-    new Header($header, {});
-<<<<<<< HEAD
->>>>>>> db879c9 (feat: add logo, nightmode btn)
-=======
->>>>>>> 09299bc (feat: add logo, nightmode btn)
-=======
     const $board = this.$target.querySelector('[data-component="Board"]');
     new Board($board, {});
->>>>>>> 01fb56f (style : all style change)
   }
 }
 
