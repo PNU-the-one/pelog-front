489bff363727d9a6091b10315529fb9958e34f77
vgihan <jum654@naver.com>
2022-05-31 21:06:24 +0900

    fix: lint error fix

--- src/page/main.ts
+++ src/page/main.ts
@@ -1,18 +1,17 @@
 import Board from "component/Board/board";
 import Component from "../core/Component";
 
-class Main extends Component{
-  template(){
-    return `<div data-component='Board'></div>`;
+class Main extends Component {
+  template() {
+    return "<div data-component='Board'></div>";
   }
-  mounted(){
-    const $board = this.$target.querySelector('[data-component="Board"]');
+
+  mounted() {
+    const $board = this.$target.querySelector("[data-component='Board']");
     new Board($board, {});
   }
-  setEvent(){
 
-  }
-  
+  setEvent() {}
 }
 
 export default Main;
