c89840c51ab38e40551d20193e0e4e4a5420f02e
vgihan <jum654@naver.com>
2022-06-01 18:34:52 +0900

    feat: add searchedPost Component
    - 검색 창에 글을 검색했을 때 보여지는 컴포넌트 구현

--- src/component/Searching/SearchResult/index.ts
+++ src/component/Searching/SearchResult/index.ts
@@ -1,3 +1,4 @@
+import SearchedPost from "component/SearchedPost";
 import Component from "core/Component";
 
 class SearchResult extends Component {
@@ -7,9 +8,26 @@ class SearchResult extends Component {
     return `
       <div id="searchresult">
         <p> 총 ${0}개의 포스트를 찾았습니다.
-        
+        <div></div>
       </div>
     `;
+  }
+
+  mounted(): void {
+    const $searchresult = this.$target.querySelector("#searchresult > div");
+
+    new SearchedPost($searchresult, {
+      writer: "vgihan",
+      title: "배열 렌더링하기 !!",
+      content:
+        "자 오늘은 배열 렌더링 하는 법에 대해 알아볼게요 ! 배열 렌더링 요즘 아주 중요한 개념인데요 ! 배열을 렌더링 하는 거에요 ! 나머지는 다음시간에 알아볼게요 !",
+      date: {
+        year: 2022,
+        month: 5,
+        day: 20,
+      },
+      comment: 10,
+    });
   }
 }
 
